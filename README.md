04e7592aca4305a4976370d7c02ec10e40c7eba9
Yuko <yuko.gerheim@gmail.com>
2023-12-23 23:13:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# My JavaScript Final Project 4 🚀✨
+
+## Welcome to my JavaScript/TypeScript project repository 👋
+
+### 🐂 Bulls and Cows 🐄
+
+This project encapsulates the classic Bulls and Cows guessing game in a digital format.  
+The game tests your deductive skills and challenges you to guess a secret 4-digit number.  
+Inspired by the traditional pen-and-paper game, this digital version brings a console-based experience to life.
+
+<img src="./img/readme_image1.jpg" alt="Bulls and Cows screenshot" width="400">
+
+### Tools I used
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=javascript) JavaScript
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=typescript) TypeScript
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=npm) npm
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=node.js) Node.js
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=Linux) Linux
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=Git) Git
+- ![Static Badge](https://img.shields.io/badge/-ffffff?style=social&logo=Github) GitHub
+
+### TypeScript Significance
+TypeScript's use significantly enhanced this project's robustness and maintainability.   
+Its static typing capabilities caught potential errors during development, improving code quality and reducing bugs.   
+Leveraging TypeScript's features boosted readability and maintainability, making it a vital aspect of the development process.
+
+### Features
+✅ Interactive Console Game: Engage in an entertaining guessing game right in your console  
+✅ Strategic Guesses: Apply deduction and logic to make precise guesses and crack the secret number  
+✅ Feedback and Hints: Receive informative hints to guide you towards the secret number  
+✅ Error Handling: Prompt notifications for invalid inputs, repeated digits, or incorrect number format to enhance your gaming experience  
+✅ Continual Play: Keep playing until you successfully decode the entire secret number  
+
+### Libraries I used:
+- chalk: Adds color and style to the console output for a better visual experience   
+- prompt-sync & readline-sync: Facilitates user input in the console   
+
+<img src="./img/readme_image2.jpg" alt="Bulls and Cows screenshot" width="400">   
+
+Feel free to personalize and elevate the game💥   
+Enjoy the challenge, and may your deductive skills bring you thrilling victories in the world of Bulls and Cows🐂🐄💕
---
+++ input/index.ts
@@ -0,0 +1,158 @@
+import promptSync from "prompt-sync";
+const prompt = promptSync({sigint: true});
+import chalk from "chalk";
+import readlineSync from "readline-sync";
+
+const playGame = (): void => {
+
+  // generate secret number
+  const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const generateSecretNumber = () => {
+    let randomNumber = "";
+
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * numbers.length);
+      randomNumber += numbers[randomIndex];
+      numbers.splice(randomIndex, 1);
+    }
+    return randomNumber;
+  };
+
+  let secretNumber = generateSecretNumber();
+  let attempts = 0;
+  console.log(secretNumber);
+
+  // validate input
+  const validateInput = (input: string) => {
+    if (input.length !== 4) {
+      return chalk.bgWhiteBright(
+        chalk.italic.redBright("\nInvalid input") +
+          "⛔ " +
+          chalk.cyanBright("Your guess must be 4-digit number!\n")
+      );
+    }
+
+    const digits: string[] = [];
+
+    for (let i = 0; i < input.length; i++) {
+      const digit = input[i];
+
+      if (digits.includes(digit)) {
+        return chalk.bgWhiteBright(
+          chalk.italic.redBright("\nAttention") +
+            "🚩 " +
+            chalk.cyanBright("Your guess should not contain repeated digits!\n")
+        );
+      }
+
+      digits.push(digit);
+
+      if (isNaN(Number(digit))) {
+        return chalk.bgWhiteBright(
+          chalk.italic.redBright("\nImportant note") +
+            "🪧  " +
+            chalk.cyanBright("Your guess must consist of digits only!\n")
+        );
+      }
+    }
+    return true;
+  };
+
+  // display hint
+  const displayHint = (secretNumber: string, guessedNumber: string) => {
+    let bulls = 0;
+    let cows = 0;
+
+    for (let i = 0; i < secretNumber.length; i++) {
+      const secretDigit = secretNumber[i];
+      const guessedDigit = guessedNumber[i];
+
+      if (secretDigit === guessedDigit) {
+        bulls++;
+      } else if (secretNumber.includes(guessedDigit)) {
+        cows++;
+      }
+    }
+
+    let bullsText = bulls === 0 ? "bull" : "bulls";
+    let cowsText = cows === 0 ? "cows" : "cows";
+    return (
+      chalk.yellowBright(" Hint:") +
+      chalk.whiteBright(` 🐂 ${bulls} ${bullsText} and 🐄 ${cows} ${cowsText}
+🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹\n`)
+    );
+  };
+
+  // main game
+  console.clear();
+  console.log(
+    chalk.green.bold("🔸🔹🐂 Welcome to the Bulls and Cows 🐄🔹🔸\n")
+  );
+  console.log(
+    chalk.bgWhiteBright.redBright(" Can you guess the secret 4-digit number?\n")
+  );
+  console.log(chalk.yellowBright(" 🎮 How to Play:"));
+  console.log(
+    chalk.whiteBright(
+      `
+ 1. Guess a 4-digit secret number
+ 2. Ensure no repeated digits in your guess
+ 3. You'll get hints
+    🐂bulls: correct digits in the right position
+    🐄cows: correct digits in the wrong position
+ 4. Win by guessing all 4 digits correctly\n
+🔸🔹🔸🔸🔹🔸🔸🔹🔸🔸🔹🔸🔸🔹🔸🔸🔹🔸🔸🔹🔸🔹🔸🔹🔸
+    ` + chalk.cyanBright(" \n🚀 Ready to play?\n")
+    )
+  );
+
+  let isGuessCorrect = false;
+  while (!isGuessCorrect) {
+    const guessedNumber = prompt(
+      chalk.whiteBright("   Enter your 4-digit guess: ")
+    );
+
+    const validationResult = validateInput(guessedNumber);
+    if (validationResult !== true) {
+      console.log(validationResult);
+      continue;
+    }
+
+    attempts++;
+
+    console.log("\n🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹");
+    console.log(
+      chalk.yellowBright(` Your guess: ${chalk.whiteBright(guessedNumber)}`)
+    );
+    console.log(
+      chalk.yellowBright(` Attempts: ${chalk.whiteBright(attempts)}`)
+    );
+    const hint = displayHint(secretNumber, guessedNumber);
+    console.log(hint);
+
+    if (guessedNumber === secretNumber) {
+      isGuessCorrect = true;
+      console.log(
+        chalk.redBright("✨🎊Congratulations🎊✨") +
+          chalk.whiteBright(
+            `\nYou guessed the secret number in ${attempts} attempts💪✨\n`
+          )
+      );
+    } else {
+      console.log(chalk.cyanBright("🎯 Keep going! You're doing great!\n"));
+    }
+  }
+
+  const playAgain = readlineSync.keyInYN(
+    chalk.bgWhiteBright.redBright("\nDo you want to play again?")
+  );
+
+  if (playAgain) {
+    playGame();
+  } else {
+    console.log(chalk.whiteBright("\nWell done! See you next time👋\n"));
+  }
+};
+
+playGame();
